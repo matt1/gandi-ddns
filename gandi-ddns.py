a798138549aa218ef65bb0ceace92e4ff57c0052
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2016-06-26 12:13:41 +0200

    Use an external configuration file
    Instead of storing the private credential in the script, it's better
    having the credential stored in a private file, not managed by git.

--- gandi-ddns.py
+++ gandi-ddns.py
@@ -2,52 +2,50 @@ from __future__ import print_function
 try:
   from xmlrpc import client as xmlrpclient
   from urllib.request import urlopen
+  import configparser
 except ImportError:
   from urllib2 import urlopen
   import xmlrpclib as xmlrpclient
+  import ConfigParser as configparser
 import sys
+import os
 
-# gandi.net API (Production) key
-apikey = '<CHANGE ME>'
-# Domain
-domain = '<CHANGE ME>'
-# A-record name
-a_name = '@'
-# TTL (seconds = 5 mintes to 30 days)
-ttl = 900
-# Production API
-api = xmlrpclient.ServerProxy('https://rpc.gandi.net/xmlrpc/', verbose=False)
 # Used to cache the zone_id for future calls
 zone_id = None
 
-def get_zone_id():
+config_file = "config.txt"
+
+def get_zone_id(config, section):
 	""" Get the gandi.net ID for the current zone version"""
-	
+
 	global zone_id
 	
 	# If we've not already got the zone ID, get it
 	if zone_id is None:
 		# Get domain info then check for a zone
-		domain_info = api.domain.info(apikey,domain)
+		domain_info = api.domain.info(config.get(section, "apikey"),
+                                  config.get(section, "domain"))
 		current_zone_id = domain_info['zone_id']
 
 		if current_zone_id == 'None':
 		  print('No zone - make sure domain is set to use gandi.net name servers.')
 		  sys.exit(1)
-		
+
 		zone_id = current_zone_id
 	
 	return zone_id
 
-def get_zone_ip():
+def get_zone_ip(config, section):
 	"""Get the current IP from the A record in the DNS zone """
 
-	current_zone = api.domain.zone.record.list(apikey, get_zone_id(), 0)
+	current_zone = api.domain.zone.record.list(config.get(section, "apikey"),
+                                             get_zone_id(config, section),
+                                             0)
 	ip = '0.0.0.0'
 	# There may be more than one A record - we're interested in one with 
 	# the specific name (typically @ but could be sub domain)
 	for d in current_zone:
-	  if d['type'] == 'A'and d['name'] == a_name:
+	  if d['type'] == 'A'and d['name'] == config.get(section, "a_name"):
 	    ip = d['value']
 	  
 	return ip
@@ -64,34 +62,67 @@ def get_ip():
 	
 	return result.decode()
 
-def change_zone_ip(new_ip):
+def change_zone_ip(config, section, new_ip):
   """ Change the zone record to the new IP """
+  a_name = config.get(section, "a_name")
+  apikey = config.get(section, "apikey")
+  ttl = int(config.get(section, "ttl"))
+  zone_id = get_zone_id(config, section)
   
   zone_record ={'name': a_name, 'value': new_ip, 'ttl':ttl, 'type': 'A'}
   
-  new_zone_ver = api.domain.zone.version.new(apikey, get_zone_id())
+  new_zone_ver = api.domain.zone.version.new(apikey, zone_id)
   
   # clear old A record (defaults to previous verison's
-  api.domain.zone.record.delete(apikey, get_zone_id(), new_zone_ver,{'type':'A', 'name': a_name})
+  api.domain.zone.record.delete(apikey, zone_id, new_zone_ver, {'type':'A', 'name': a_name})
   
   # Add in new A record
-  api.domain.zone.record.add(apikey, get_zone_id(), new_zone_ver, zone_record)
+  api.domain.zone.record.add(apikey, zone_id, new_zone_ver, zone_record)
   
   # Set new zone version as the active zone
-  api.domain.zone.version.set(apikey, get_zone_id(), new_zone_ver)
+  api.domain.zone.version.set(apikey, zone_id, new_zone_ver)
 
+def read_config(config_path):
+  """ Open the configuration file or create it if it doesn't exists """
+  if not os.path.exists(config_path):
+    with open(config_path, "w") as f:
+      f.write("""[local]
+# gandi.net API (Production) key
+apikey = <CHANGE ME>
+# Domain
+domain = <CHANGE ME>
+# A-record name
+a_name = @
+# TTL (seconds = 5 mintes to 30 days)
+ttl = 900
+# Production API
+api = https://rpc.gandi.net/xmlrpc/
+""")
+    return None
+  cfg = configparser.ConfigParser()
+  cfg.read(config_path)
+  return cfg
 
 def main():
-  zone_ip = get_zone_ip()
+  global api, zone_id
+  config = read_config(config_file)
+  if not config:
+    sys.exit("please fill in the 'config.txt' file")
+
+  section = config.sections()[0]
+
+  api = xmlrpclient.ServerProxy(config.get(section, "api"), verbose=False)
+
+  zone_ip = get_zone_ip(config, section).strip()
   current_ip = get_ip()
 
   if (zone_ip.strip() == current_ip.strip()):
     sys.exit();
   else:
     print('DNS Mistmatch detected: A-record: ', zone_ip, ' WAN IP: ', current_ip)
-    change_zone_ip(current_ip)
+    change_zone_ip(config, section, current_ip)
     zone_id = None
-    zone_ip = get_zone_ip();
+    zone_ip = get_zone_ip(config, section);
     print('DNS A record update complete - set to ', zone_ip)
 
 if __name__ == "__main__":
